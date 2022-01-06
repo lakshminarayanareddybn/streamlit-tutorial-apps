7bbb97925e1ac9868b89df8bb6f6bf91b30a7389
Lakshminarayana reddy <lakshminarayana84.reddy@gmail.com>
2022-01-06 13:59:27 +0530

    Update streamlit_hello_world.py

--- streamlit_hello_world.py
+++ streamlit_hello_world.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.write("Here's our first attempt at using data to create a table:")
+st.write("LNR: Here's your first attempt to use streamlit cloud to host application")
 st.write(pd.DataFrame({
     'first column': [1, 2, 3, 4],
     'second column': [10, 20, 30, 40]
